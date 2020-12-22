d8edf52c96275ce3693c8315607e8a4b121aa996
Christian <christian_d_ol9000@hotmail.com>
2020-12-22 16:05:22 -0300

    dia 17

---
+++ dia 17/classes/Produto.cs
@@ -0,0 +1,19 @@
+namespace dia_17.classes
+{
+    public class Produto
+    {
+          public int Codigo{ get; set;}
+        public string Nome{ get; set;}
+        public float Preço{ get; set;}
+        public string P {get; set;}
+
+    
+        public Produto(string p,int cod, string nom, float prec)
+        {
+            P = p;
+            Codigo = cod;
+            Nome = nom;
+            Preço = prec;
+        }
+    }
+}
